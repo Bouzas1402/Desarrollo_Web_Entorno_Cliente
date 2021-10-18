fdc8db5ae32f03bec3eba18ebaa448288d37a16a
Bouzas <carlos.bouzas@educa.madrid.or>
2021-10-18 18:37:12 +0200

    18:36 18/10/2021

---
+++ Tema-2/Practicas/bisiestos/bisiestos.js
@@ -0,0 +1,39 @@
+var Bisiestos = Bisiestos || {};
+
+Bisiestos = function()
+{
+    /**
+     *  Private
+     */
+    var version = '1.0';
+
+    function init() {
+
+    }
+
+    function calcularBisiesto(){
+
+
+
+
+    }
+
+
+    /**
+     *  Ejecutable al cargar la pagina:
+     */
+
+    init();
+
+    /**
+     *  Public
+     */
+
+    return {
+        version: function(){
+            return version;
+        }
+        ,init: init
+        ,calcularBisiesto: calcularBisiesto
+    }
+};
