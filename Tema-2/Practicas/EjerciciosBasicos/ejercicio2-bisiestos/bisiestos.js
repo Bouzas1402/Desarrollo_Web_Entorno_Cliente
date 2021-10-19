ec6e98656b5f1811f716db0a25fd76451c088295
Bouzas1402 <72886309+Bouzas1402@users.noreply.github.com>
2021-10-19 14:54:06 +0200

    19/10/2021 14:54

--- Tema-2/Practicas/EjerciciosBasicos/ejercicio2-bisiestos/bisiestos.js
+++ Tema-2/Practicas/EjerciciosBasicos/ejercicio2-bisiestos/bisiestos.js
@@ -11,13 +11,13 @@ Bisiestos = function()
 
     }
 
-    function calcularBisiesto(){
-
-
-
-
+    function calcularBisiesto(ano){
+        if (((ano % 4) === 0 && (ano % 100) !== 0) || ((ano % 4) === 0 && (ano % 100) === 0 && (ano % 400) === 0)) {
+            return ano + " es año bisiesto";
+        } else  {
+            return ano + "No es un año bisiesto";
+        }
     }
-
 
     /**
      *  Ejecutable al cargar la pagina:
