2350c954861b31a993b1482b70cf43c6da8014af
Bouzas1402 <72886309+Bouzas1402@users.noreply.github.com>
2021-11-16 11:14:13 +0100

    16/11/2021 14:54

---
+++ Tema-3/Practicas/Ejercicio 3 - Manejo de ventanas/padre.js
@@ -0,0 +1,25 @@
+const $btnAbrir = document.querySelector("#btnAbrir"),
+	$mensaje = document.querySelector("#mensaje"),
+	$btnEnviarMensaje = document.querySelector("#btnEnviarMensaje"),
+	$mensajeRecibido = document.querySelector("#mensajeRecibido");
+
+let ventana;
+$btnAbrir.addEventListener("click", () => {
+	ventana = window.open("ventana.html","Ventana hija","height=400,width=800");
+	ventana.console.log("hola");
+});
+
+$btnEnviarMensaje.addEventListener("click", () => {
+	let mensaje = $mensaje.value;
+	if (!mensaje) {
+		return;
+	}
+	if (ventana) {
+		ventana.establecerMensaje(mensaje);
+	}
+});
+
+// Llamada desde la hija
+function establecerMensaje(mensaje) {
+	$mensajeRecibido.textContent = mensaje;
+}
