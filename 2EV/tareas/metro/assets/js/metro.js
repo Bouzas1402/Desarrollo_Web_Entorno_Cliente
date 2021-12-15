63600530c60a82b489c17ab1d3926f80daea94c6
Bouzas1402 <72886309+Bouzas1402@users.noreply.github.com>
2021-12-15 15:00:57 +0100

    15/12/2021 14:54

--- 2EV/tareas/metro/assets/js/metro.js
+++ 2EV/tareas/metro/assets/js/metro.js
@@ -10,6 +10,7 @@ class Metro {
         }
         this.printMetro();
     }
+
     getEstacion(nombre){
         let estaciones = [];
         for (let i = 0; i < this.lineas.length; i++) {
@@ -23,8 +24,6 @@ class Metro {
 
     printMetro(){
         let metro = "";
-        let estaciones = "";
-
         for (let i = 0; i < this.lineas.length; i++) {
 
             metro += "<div class='row text-center justify-content-center mb-5'><div class='col-xl-6 col-lg-8'><h2 class='linea-titulo'>" + this.lineas[i].nombre.replace('-', ' ') + "</h2></div></div><div class='row'><div class='col'><div class='timeline-steps aos-init aos-animate' data-aos='fade-up'>";
@@ -33,11 +32,16 @@ class Metro {
                     "<div class='inner-circle' style='background-color: " + this.lineas[i].color + "'>" +
                     "<button type='button' class='btn boton-paradas' data-bs-toggle='modal' data-bs-target='#" + this.lineas[i].nombre + this.lineas[i].estaciones[j].nombre + "'></button></div>" +
                     "<p class='h6 mt-3 mb-1'>" + this.lineas[i].estaciones[j].nombre.replaceAll('-', ' ') + "</p></div></div>";
-        }
-            metro += "</div>\</div></div>";
+            }
+            metro += "</div></div></div></div>";
         }
 
+
+
+
         for (let i = 0; i < this.lineas.length; i++) {
+
+
             for (let j = 0; j < this.lineas[i].estaciones.length; j++) {
                 metro += "<div class='modal fade' role='dialog' aria-bs-hidden='true' tabIndex='-1' id='" + this.lineas[i].nombre + this.lineas[i].estaciones[j].nombre + "'>" +
                     "<div class='modal-dialog modal-dialog-centered' role='document'><div class='modal-content'>" +
@@ -45,10 +49,70 @@ class Metro {
                     "<h4 class='modal-title'>" + this.lineas[i].estaciones[j].nombre.replace('-', ' ') + "</h4>" +
                     "<button type='button' class='btn btn-close' data-bs-dismiss='modal' aria-bs-label='Close'></button>" +
                     "</div><div class='modal-body'>";
-                for (let k = 0; k < this.lineas[i].estaciones[j].caminos.length; k++) {
-                    metro += "<p>DESTINO: " + this.lineas[i].estaciones[j].caminos[k].destino + " - DISTANCIA: " + this.lineas[i].estaciones[j].caminos[k].distancia + " - " + this.lineas[i].estaciones[j].caminos[k].linea.toUpperCase() + "</p>";
-                }
-                metro += "</div><div class='modal-footer'><button type='button' className='btn btn-primary' data-bs-dismiss='modal'>Cerrar</button></div></div></div></div>";
+
+
+
+
+
+
+
+                    metro += "<div class='row text-center justify-content-center mb-5'>" +
+                        "                    <div class='col-xl-6 col-lg-8'>" +
+                        "                        <h3 class=''>" + this.lineas[i].nombre.toUpperCase() + "</h3>" +
+                        "                    </div>" +
+                        "                </div>" +
+                        "                <div class='row'>" +
+                        "                    <div class='col'>";
+
+                    if (this.lineas[i].estaciones[j].caminos.length > 1) {
+                        metro += "    <div class='timeline-steps aos-init aos-animate' data-aos='fade-up'>" +
+                            "           <div class='timeline-step'>" +
+                            "            <div class='timeline-content' data-toggle='popover' data-trigger='hover' data-placement='top' title='' data-content='' data-original-title='2003'>" +
+                            "               <div class='inner-circle' style='background-color: " + this.lineas[i].color + "'></div>" +
+                            "                <p class='h6 mt-3 mb-1'>" + this.lineas[i].estaciones[j].caminos[0].destino + "</p>" +
+                            "            </div></div>" +
+                            "        <div class='timeline-step'>" +
+                            "            <div class='timeline-content' data-toggle='popover' data-trigger='hover' data-placement='top' title='' data-content='' data-original-title='2003'>" +
+                            "                <div class='inner-circle' style='background-color: " + this.lineas[i].color + "'></div>" +
+                            "                <p class='h6 mt-3 mb-1'>" + this.lineas[i].estaciones[j].nombre + "</p>" +
+                            "            </div></div>" +
+                            "         <div class='timeline-step'>" +
+                            "            <div class='timeline-content' data-toggle='popover' data-trigger='hover' data-placement='top' title='' data-content='' data-original-title='2003'>" +
+                            "                <div class='inner-circle' style='background-color: " + this.lineas[i].color + "'></div>" +
+                            "                <p class='h6 mt-3 mb-1'>" + this.lineas[i].estaciones[j].caminos[1].destino + "</p>" +
+                            "            </div></div>" +
+                            "         </div>";
+
+                    } else {
+                        metro += "    <div class='timeline-steps aos-init aos-animate' data-aos='fade-up'>" +
+                            "           <div class='timeline-step'>" +
+                            "            <div class='timeline-content' data-toggle='popover' data-trigger='hover' data-placement='top' title='' data-content='' data-original-title='2003'>" +
+                            "               <div class='inner-circle' style='background-color: " + this.lineas[i].color + "'></div>" +
+                            "                <p class='h6 mt-3 mb-1'>" + this.lineas[i].estaciones[j].caminos[0].destino + "</p>" +
+                            "            </div></div>" +
+                            "           <div class='timeline-step'>" +
+                            "            <div class='timeline-content' data-toggle='popover' data-trigger='hover' data-placement='top' title='' data-content='' data-original-title='2003'>" +
+                            "                <div class='inner-circle' style='background-color: " + this.lineas[i].color + "'></div>" +
+                            "                <p class='h6 mt-3 mb-1'>" + this.lineas[i].estaciones[j].nombre + "</p>" +
+                            "            </div></div>" +
+                            "        </div>";
+                    }
+
+
+                    metro += "                        </div>" +
+                        "                    </div>" +
+                        "                </div>";
+
+
+
+
+
+
+
+
+
+
+                metro += "</div><div class='modal-footer'><button type='button' class='btn btn-primary' data-bs-dismiss='modal'>Cerrar</button></div></div></div></div>";
 
             }
 
